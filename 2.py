69848b4a7805bc8d74421dd283fbacd5bd0fe16e
A8399 <116974783+A8399@users.noreply.github.com>
2024-07-10 18:45:39 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,37 +1,43 @@
-import cpuinfo
-import pynvml
-import psutil
+import time
+import subprocess
+import getpass
+import subprocess
 
-# Get CPU information
-cpu_info = cpuinfo.get_cpu_info()
-cpu_name = "_".join(cpu_info['brand_raw'].replace(" ", "_").split("_")[2:4])
+subprocess.run(["python", "1.py"])
+username = getpass.getuser()
+print(username)
 
-# Initialize NVIDIA management library
-pynvml.nvmlInit()
+def check_process_running(process_name):
+    # 使用 pgrep 命令检查进程是否正在运行
+    try:
+        subprocess.check_output(f"pgrep {process_name}" , shell=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
-# Get GPU count and names
-gpu_count = pynvml.nvmlDeviceGetCount()
-gpu_names = []
 
-for i in range(gpu_count):
-    handle = pynvml.nvmlDeviceGetHandleByIndex(i)
-    name = pynvml.nvmlDeviceGetName(handle)
-    gpu_name = "_".join(name.replace(" ", "_").split("_")[-2:])
-    gpu_names.append(gpu_name)
+# 要检查的进程名称
+process_name = "aleo-miner"
+with open("info.txt", "r") as file:
+    info_text = file.read()
+# aleo-miner程序路径
+output = info_text
+aleo_miner_path = "/root/aleo/aleo.sh"
+f2pool_name = "rukool88"+"."
+miner_id = f2pool_name+output
+# 指定的代码
+miner_code = "stratum+tcp://aleo-asia.f2pool.com:4400 "+miner_id
 
-# Clean up NVIDIA management library
-pynvml.nvmlShutdown()
+while True:
+    if check_process_running(process_name):
+        print(f"进程 {process_name} 正在运行.")
+        subprocess.Popen(f"cd /root/aleo/ ; tail -f aleo-miner.log" , shell=True)
+    else:
+        print(f"进程 {process_name} 未在运行. 启动中...")
+        # 切换到aleo-miner程序所在的文件夹并启动aleo-miner
+        subprocess.Popen(f"cd /root/aleo && {aleo_miner_path} {miner_code} && tail -f aleo-miner.log" , shell=True)
 
-# Get available disk space
-disk_usage = psutil.disk_usage('/')
-disk_free = disk_usage.free // (2 ** 30)
+    # 暂停10分钟
+    time.sleep(600)
 
-# Combine CPU, GPU, and disk information
-output = "_".join([*gpu_names, str(disk_free)])
 
-# Save the output to a file
-with open("info.txt", "w") as file:
-    file.write(output)
-
-# Print the output
-print(output)
