150838a81a8562c334531991716806304ab73185
A8399 <116974783+A8399@users.noreply.github.com>
2024-07-10 17:56:48 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -16,7 +16,8 @@ gpu_names = []
 for i in range(gpu_count):
     handle = pynvml.nvmlDeviceGetHandleByIndex(i)
     name = pynvml.nvmlDeviceGetName(handle)
-    gpu_names.append(name)
+    gpu_name = name.decode()
+    gpu_names.append(gpu_name)
 
 # 清理 NVIDIA 管理库
 pynvml.nvmlShutdown()
@@ -25,25 +26,13 @@ pynvml.nvmlShutdown()
 disk_usage = psutil.disk_usage('/')
 disk_free = disk_usage.free // (2**30)
 
-# 构建输出字符串
-output_str = "{}_{}_{}".format(cpu_name.replace(" ", "-"), "_".join(gpu_names), disk_free)
+# 打印 CPU 名称
+print("CPU 名称:", cpu_name)
 
-# 打印输出字符串
-print(output_str)
+# 打印 GPU 数量和名称
+print("GPU 数量:", gpu_count)
+for i, gpu_name in enumerate(gpu_names):
+    print("GPU", i, "名称:", gpu_name)
 
-text = output_str
-
-# 提取 CPU 名称
-cpu_name = text.split("_")[0].split("-")[3]
-
-# 提取 GPU 名称和硬盘剩余容量
-gpu_disk_info = text.split("_")[1:]
-gpu_name = gpu_disk_info[0]
-disk_free = gpu_disk_info[1]
-
-gpu = gpu_name.split(" ")[-1]
-# 构建输出字符串
-output_str = "{}_{}_{}".format(cpu_name, gpu, disk_free)
-
-# 打印输出字符串
-print(output_str)
+# 打印硬盘剩余容量
+print("硬盘剩余容量:", disk_free, "GB")
