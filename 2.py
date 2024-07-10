71adbb3b70c9b4db960371c481e95997f2c7d2ee
A8399 <116974783+A8399@users.noreply.github.com>
2024-07-10 17:26:43 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,36 +1,37 @@
-import time
-import subprocess
-import getpass
+import cpuinfo
+import pynvml
+import psutil
 
-username = getpass.getuser()
-print(username)
+# 获取 CPU 信息
+cpu_info = cpuinfo.get_cpu_info()
+cpu_name = cpu_info['brand_raw']
 
-def check_process_running(process_name):
-    # 使用 pgrep 命令检查进程是否正在运行
-    try:
-        subprocess.check_output(f"pgrep {process_name}" , shell=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
+# 初始化 NVIDIA 管理库
+pynvml.nvmlInit()
 
+# 获取 GPU 数量和名称
+gpu_count = pynvml.nvmlDeviceGetCount()
+gpu_names = []
 
-# 要检查的进程名称
-process_name = "aleo-miner"
-# aleo-miner程序路径
-aleo_miner_path = "/root/aleo/aleo.sh"
-# 指定的代码
-miner_code = "stratum+tcp://aleo-asia.f2pool.com:4400 rukool88.1999999"
+for i in range(gpu_count):
+    handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+    name = pynvml.nvmlDeviceGetName(handle)
+    gpu_names.append(name.decode())
 
-while True:
-    if check_process_running(process_name):
-        print(f"进程 {process_name} 正在运行.")
-        subprocess.Popen(f"cd /root/aleo/ ; tail -f aleo-miner.log" , shell=True)
-    else:
-        print(f"进程 {process_name} 未在运行. 启动中...")
-        # 切换到aleo-miner程序所在的文件夹并启动aleo-miner
-        subprocess.Popen(f"cd /root/aleo && {aleo_miner_path} {miner_code} && tail -f aleo-miner.log" , shell=True)
+# 清理 NVIDIA 管理库
+pynvml.nvmlShutdown()
 
-    # 暂停10分钟
-    time.sleep(600)
+# 获取硬盘剩余容量
+disk_usage = psutil.disk_usage('/')
+disk_free = disk_usage.free // (2**30)
 
+# 打印 CPU 名称
+print("CPU 名称:", cpu_name)
 
+# 打印 GPU 数量和名称
+print("GPU 数量:", gpu_count)
+for i, gpu_name in enumerate(gpu_names):
+    print("GPU", i, "名称:", gpu_name)
+
+# 打印硬盘剩余容量
+print("硬盘剩余容量:", disk_free, "GB")
