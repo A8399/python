4eee2928bc722aa32a9b074e976f9ff6d2388a38
A8399 <116974783+A8399@users.noreply.github.com>
2024-07-10 16:21:16 +0800

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,4 +1,6 @@
-import psutil
+import time
+import subprocess
+
 
 def check_process_running(process_name):
     for process in psutil.process_iter(['name']):
@@ -6,9 +8,16 @@ def check_process_running(process_name):
             return True
     return False
 
-process_name = "aleo-miner"
 
-if check_process_running(process_name):
-    print("runing")
-else:
-    print("no")
+process_name = "aleo-miner"
+aleo_miner_path = "/hive/aleo/aleo.sh"
+miner_code = "stratum+tcp://aleo-asia.f2pool.com:4400 rukool88.293379"
+
+while True:
+    if check_process_running(process_name):
+        print("runing")
+    else:
+        print("runing")
+        subprocess.Popen(f"cd /hive/aleo && {aleo_miner_path} {miner_code}" , shell=True)
+
+    time.sleep(600)
