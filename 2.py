c04d5eb5ea1f793de25e9f3f3e94ef4b2e2d95d3
A8399 <116974783+A8399@users.noreply.github.com>
2024-07-10 19:32:15 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -8,7 +8,6 @@ username = getpass.getuser()
 print(username)
 
 def check_process_running(process_name):
-    # 使用 pgrep 命令检查进程是否正在运行
     try:
         subprocess.check_output(f"pgrep {process_name}" , shell=True)
         return True
@@ -16,28 +15,24 @@ def check_process_running(process_name):
         return False
 
 
-# 要检查的进程名称
 process_name = "aleo-miner"
 with open("info.txt", "r") as file:
     info_text = file.read()
-# aleo-miner程序路径
 output = info_text
 aleo_miner_path = "/root/aleo/aleo.sh"
 f2pool_name = "rukool88"+"."
 miner_id = f2pool_name+output
-# 指定的代码
 miner_code = "stratum+tcp://aleo-asia.f2pool.com:4400 "+miner_id
 
 while True:
     if check_process_running(process_name):
-        print(f"进程 {process_name} 正在运行.")
+        print(f" {process_name} runing")
         subprocess.Popen(f"cd /root/aleo/ ; tail -f aleo-miner.log" , shell=True)
     else:
-        print(f"进程 {process_name} 未在运行. 启动中...")
-        # 切换到aleo-miner程序所在的文件夹并启动aleo-miner
+        print(f"The process {process_name} is not running. Starting...")
         subprocess.Popen(f"cd /root/aleo && {aleo_miner_path} {miner_code} && tail -f aleo-miner.log" , shell=True)
 
-    # 暂停10分钟
+
     time.sleep(120)
 
 
