68b1fb97eb35e8c8ec0164be011f7717926bb451
A8399 <116974783+A8399@users.noreply.github.com>
2024-07-10 16:25:17 +0800

    Add files via upload

--- 1.py
+++ 1.py
@@ -9,15 +9,20 @@ def check_process_running(process_name):
     return False
 
 
+# 要检查的进程名称
 process_name = "aleo-miner"
+# aleo-miner程序路径
 aleo_miner_path = "/hive/aleo/aleo.sh"
+# 指定的代码
 miner_code = "stratum+tcp://aleo-asia.f2pool.com:4400 rukool88.293379"
 
 while True:
     if check_process_running(process_name):
-        print("runing")
+        print(f"进程 {process_name} 正在运行.")
     else:
-        print("no")
-        subprocess.Popen("cd /hive/aleo && {} {}".format(aleo_miner_path , miner_code) , shell=True)
+        print(f"进程 {process_name} 未在运行. 启动中...")
+        # 切换到aleo-miner程序所在的文件夹
+        subprocess.Popen(f"cd /hive/aleo && {aleo_miner_path} {miner_code}" , shell=True)
 
+    # 暂停10分钟
     time.sleep(600)
