45c9df714c2b766c78a1ae0164122926547e2be9
A8399 <116974783+A8399@users.noreply.github.com>
2024-07-11 19:00:52 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -29,9 +29,6 @@ process_name = "aleo-miner"
 aleo_miner_path = "/root/aleo/aleo.sh"
 miner_code = "stratum+tcp://aleo-asia.f2pool.com:4400 " + miner_id
 
-# 设定定时重新启动的时间间隔（单位：秒）
-restart_interval = 7200  # 两小时
-
 while True:
     if not check_process_running(process_name):
         print(f"{process_name} 进程未运行，启动进程...")
@@ -40,16 +37,5 @@ while True:
     else:
         print(f"{process_name} 进程正在运行")
 
-    # 等待重新启动的时间间隔
-    print(f"等待 {restart_interval} 秒后重新启动 {process_name} 进程...")
-    time.sleep(restart_interval)
-
-    # 关闭进程
-    print(f"关闭 {process_name} 进程...")
-    stop_process(process_name)
-    time.sleep(10)  # 等待一段时间确保进程关闭完全
-
-    # 启动进程
-    print(f"重新启动 {process_name} 进程...")
-    start_process(f"cd /root/aleo && nohup {aleo_miner_path} {miner_code} > aleo-miner.log 2>&1 &")
-    print(f"{process_name} 进程已重新启动")
+    # 等待5秒后进行下一次检测
+    time.sleep(5)
