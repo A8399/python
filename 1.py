a1248ec9476d3b72f9e9840eb8083dda32028682
A8399 <116974783+A8399@users.noreply.github.com>
2024-07-11 18:32:36 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -21,15 +21,16 @@ def start_process(command):
 with open("/root/info.txt", "r") as file:
     info_text = file.read()
 output = info_text
-f2pool_name = "rukool88"+"."
-miner_id = f2pool_name+output
+f2pool_name = "rukool88" + "."
+miner_id = f2pool_name + output
 process_name = "aleo-miner"
 aleo_miner_path = "/root/aleo/aleo.sh"
-miner_code = "stratum+tcp://aleo-asia.f2pool.com:4400 "+miner_id
+miner_code = "stratum+tcp://aleo-asia.f2pool.com:4400 " + miner_id
 
 # 设定定时关闭和重新打开的时间间隔（单位：秒）
 interval = 3600  # 一小时
 restart_delay = 120  # 重新启动延迟时间（单位：秒）
+startup_delay = 30  # 启动延迟时间（单位：秒）
 
 while True:
     if check_process_running(process_name):
@@ -39,6 +40,15 @@ while True:
         print(f"{process_name} 进程已关闭")
     else:
         print(f"{process_name} 进程未运行")
+
+        # 等待启动延迟时间
+        print(f"等待 {startup_delay} 秒后启动 {process_name} 进程...")
+        time.sleep(startup_delay)
+
+        # 启动进程
+        print(f"启动 {process_name} 进程...")
+        start_process(f"cd /root/aleo && {aleo_miner_path} {miner_code} && tail -f aleo-miner.log")
+        print(f"{process_name} 进程已启动")
 
     # 等待指定的时间间隔
     print(f"等待 {interval} 秒...")
